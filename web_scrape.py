06fc83664283966f802f10230d22ab25e213fb33
SciLor <scilor@users.noreply.github.com>
2023-11-03 09:32:01 +0000

    create new YAMLs if unknown content

--- web_scrape.py
+++ web_scrape.py
@@ -69,88 +69,92 @@ for lang, data in json_data.items():
         if os.path.exists(yaml_path):
             with open(yaml_path, 'r') as yaml_file:
                 yaml_base = yaml.safe_load(yaml_file)
+        else:
+            print(f"Prepare new YAML for {article}")
+            yaml_base = YamlStruct.get_base()
+            yaml_base["data"].append(YamlStruct.get_data())
              
-            if yaml_base["lock-data"]:
-                print(f'Article {article} locked, skipping update.')
-            else:
-                yaml_src_data = yaml_base["data"][0] # always take the first, most recent entry
-                yaml_data = YamlStruct.get_data()
+        if yaml_base["lock-data"]:
+            print(f'Article {article} locked, skipping update.')
+        else:
+            yaml_src_data = yaml_base["data"][0] # always take the first, most recent entry
+            yaml_data = YamlStruct.get_data()
 
-                # Define a mapping dictionary
-                field_mapping = {
-                    "series": ("series", "label"),
-                    "episode": ("name",),
-                    "release": ("publicationDate", lambda x: convert_to_int(x)),  # Remove last 3 digits (ms to s)
-                    "language": ("lcCC", lambda x: lang_cleanup(x)),  # Convert to lowercase
-                    "runtime": ("runTime",),
-                    "age": ("ageMin",),
-                    "origin": (None, "stock"),  # Fixed value
-                    "category": ("genre", "key"),
-                    "image": ("image", "src"),
-                    "sample": ("audioSampleUrl",),
-                    "web": ("path", lambda x: f'https://tonies.com{x}'),
-                    "shop-id": ("id",),
-                }
+            # Define a mapping dictionary
+            field_mapping = {
+                "series": ("series", "label"),
+                "episode": ("name",),
+                "release": ("publicationDate", lambda x: convert_to_int(x)),  # Remove last 3 digits (ms to s)
+                "language": ("lcCC", lambda x: lang_cleanup(x)),  # Convert to lowercase
+                "runtime": ("runTime",),
+                "age": ("ageMin",),
+                "origin": (None, "stock"),  # Fixed value
+                "category": ("genre", "key"),
+                "image": ("image", "src"),
+                "sample": ("audioSampleUrl",),
+                "web": ("path", lambda x: f'https://tonies.com{x}'),
+                "shop-id": ("id",),
+            }
 
-                # Map the fields based on the mapping dictionary
-                for field, source_path in field_mapping.items():
-                    if source_path[0] is None: # Fixed value
-                        yaml_data[field] = source_path[1]  # Assign the fixed value
-                    else:
-                        value = product
-                        for path_component in source_path:
-                            if callable(path_component):
-                                value = path_component(value)
-                            else:
-                                value = value.get(path_component, None)
-
-                        if value is not None:
-                            yaml_data[field] = value
-
-                cache_path = f'{Config.cache_dir}{article}.tonies.{lang}.html'
-                file_mtime = 0
-                if os.path.exists(cache_path):
-                    file_mtime = os.path.getmtime(cache_path)
-
-                if time.time() > file_mtime + 60*60*24: # Only update if older than 24h
-                    response = requests.get(yaml_data["web"])
-                    if response.status_code == 200:
-                        html_content = response.text
-                        with open(cache_path, "w") as cache_file:
-                            cache_file.write(html_content)
+            # Map the fields based on the mapping dictionary
+            for field, source_path in field_mapping.items():
+                if source_path[0] is None: # Fixed value
+                    yaml_data[field] = source_path[1]  # Assign the fixed value
                 else:
-                    with open(cache_path, "r") as cache_file:
-                        html_content = cache_file.read()
+                    value = product
+                    for path_component in source_path:
+                        if callable(path_component):
+                            value = path_component(value)
+                        else:
+                            value = value.get(path_component, None)
 
-                if yaml_data["language"] != lang: # skip duplicates that are in multiple shops
+                    if value is not None:
+                        yaml_data[field] = value
+
+            cache_path = f'{Config.cache_dir}{article}.tonies.{lang}.html'
+            file_mtime = 0
+            if os.path.exists(cache_path):
+                file_mtime = os.path.getmtime(cache_path)
+
+            if time.time() > file_mtime + 60*60*24: # Only update if older than 24h
+                response = requests.get(yaml_data["web"])
+                if response.status_code == 200:
+                    html_content = response.text
+                    with open(cache_path, "w") as cache_file:
+                        cache_file.write(html_content)
+            else:
+                with open(cache_path, "r") as cache_file:
+                    html_content = cache_file.read()
+
+            if yaml_data["language"] != lang: # skip duplicates that are in multiple shops
+                continue
+
+            match = pattern.search(html_content)
+            if match:
+                json_str = match.group(1)
+                data = json.loads(json_str)
+                try:
+                    yaml_data["track-desc"] = data["props"]["pageProps"]["product"]["tracks"]
+                except (KeyError, TypeError):
+                    pass
+            
+            updated = False
+            for attr, data in yaml_data.items():
+                if (attr == "ids"):
                     continue
+                if (yaml_src_data[attr] != data):
+                    updated = True
+                    yaml_src_data[attr] = data
 
-                match = pattern.search(html_content)
-                if match:
-                    json_str = match.group(1)
-                    data = json.loads(json_str)
-                    try:
-                        yaml_data["track-desc"] = data["props"]["pageProps"]["product"]["tracks"]
-                    except (KeyError, TypeError):
-                        pass
-                
-                updated = False
-                for attr, data in yaml_data.items():
-                    if (attr == "ids"):
-                        continue
-                    if (yaml_src_data[attr] != data):
-                        updated = True
-                        yaml_src_data[attr] = data
-
-                if updated:
-                    track_desc_len = len(yaml_src_data["track-desc"])
-                    track_len = 0
-                    if len(yaml_src_data["ids"]) > 0:
-                        track_len = yaml_src_data["ids"][0]["tracks"]
-                    if track_desc_len != track_len and track_len > 0:
-                        print(f'Article {article} track count different ({track_desc_len}/{track_len}), skipping.')
-                    else:
-                        yaml_base["last-update"] = int(time.time())
-                        with open(yaml_path, "w") as yaml_file:
-                            print(f'Updated {article}...')
-                            yaml.safe_dump(yaml_base, yaml_file, default_flow_style=False, sort_keys=False, allow_unicode=True)
+            if updated:
+                track_desc_len = len(yaml_src_data["track-desc"])
+                track_len = 0
+                if len(yaml_src_data["ids"]) > 0:
+                    track_len = yaml_src_data["ids"][0]["tracks"]
+                if track_desc_len != track_len and track_len > 0:
+                    print(f'Article {article} track count different ({track_desc_len}/{track_len}), skipping.')
+                else:
+                    yaml_base["last-update"] = int(time.time())
+                    with open(yaml_path, "w") as yaml_file:
+                        print(f'Updated {article}...')
+                        yaml.safe_dump(yaml_base, yaml_file, default_flow_style=False, sort_keys=False, allow_unicode=True)
